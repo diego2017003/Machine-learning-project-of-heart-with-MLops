f53303081b88a1c22832150fc1b352db12171b46
diego medeiros <diego.medeiros@geowellex.com>
2022-05-31 10:30:23 -0300

    update api

---
+++ source/api/main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import wandb
+import pandas as pd
+import train, preprocessing
+
+app = FastAPI()
+
+# Creating a class for the attributes input to the ML model.
+class Heart_metrics(BaseModel):
+    BMI: float
+    Smoking: str
+    AlcoholDrinking: str
+    Stroke: str
+    PhysicalHealth: float
+    MentalHealth: float
+    DiffWalking: str
+    Sex: str
+    AgeCategory: str
+    Race: str
+    Diabetic: str
+    PhysicalActivity: str
+    GenHealth: str
+    SleepTime: float
+    Asthma: str
+    KidneyDisease: str
+    SkinCancer: str
+
+    class config:
+        schema_extra = {
+            "example": {
+                "BMI": 16.4,
+                "Smoking": "Yes",
+                "AlcoholDrinking": "No",
+                "Stroke": "No",
+                "PhysicalHealth": 3.0,
+                "MentalHealth": 30.0,
+                "DiffWalking": "No",
+                "Sex": "Female",
+                "AgeCategory": "55-59",
+                "Race": "White",
+                "Diabetic": "Yes",
+                "PhysicalActivity": "Yes",
+                "GenHealth": "Very good",
+                "SleepTime": 5.0,
+                "Asthma": "Yes",
+                "KidneyDisease": "No",
+                "SkinCancer": "Yes",
+            }
+        }
+
+
+run = wandb.init(project="project_heart", job_type="api")
+
+
+@app.get("/")
+def home():
+    return {"Hello": "World"}
+
+
+@app.post("/prediction/")
+def get_potability(data: Heart_metrics):
+    artifact = run.use_artifact(
+        "diego25rm/project_heart/model_export:v1", type="pipeline_artifact"
+    ).file()
+    loaded_model = joblib.load(artifact)
+    X = pd.DataFrame([data])
+    prediction = loaded_model.predict(X)
+    return {"Prediction": prediction}
---
+++ source/app/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_agraph import agraph, Node, Edge, Config
+
+
+st.set_page_config(
+    layout="wide",
+)
+#
+form = st.form()
+form
